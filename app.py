7b17b8f71f83ea120b5afa83f127a03869025b4a
Aroun <ray.aroun@gmail..com>
2023-09-07 17:05:48 -0700

    moving pieces

--- app.py
+++ app.py
@@ -7,17 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def get_endgame_scenario():
+@app.route('/get-endgame', methods=['GET'])
+def get_endgame():
     board = chess.Board()
-    board.clear_board()
+    board.clear()
     board.set_piece_at(chess.E1, chess.Piece(chess.KING, chess.WHITE))
     board.set_piece_at(chess.E8, chess.Piece(chess.KING, chess.BLACK))
     board.set_piece_at(chess.H1, chess.Piece(chess.ROOK, chess.WHITE))
-    return board
-
-@app.route('/get-endgame', methods=['GET'])
-def get_endgame():
-    board = get_endgame_scenario()
     return jsonify({'fen': board.fen()})
 
 if __name__ == '__main__':
--- static/chess_logic.js
+++ static/chess_logic.js
@@ -3,13 +3,44 @@ var boardConfig = {
     position: 'start',
     draggable: true
 };
-var board = ChessBoard('board', {
-    position: 'start',
-    pieceTheme: '/static/img/chesspieces/wikipedia/{piece}.png'
-});
 
-fetch('/get-endgame')
-    .then(response => response.json())
-    .then(data => {
-        board.position(data.fen);
+var board,
+    game = new Chess();
+
+function onDragStart (source, piece, position, orientation) {
+    if (game.game_over()) return false;
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false;
+    }
+}
+
+function onDrop (source, target) {
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q'
     });
+    if (move === null) return 'snapback';
+    updateBoard();
+}
+
+function updateBoard() {
+    board.position(game.fen());
+}
+
+function initializeBoard() {
+    fetch('/get-endgame')
+        .then(response => response.json())
+        .then(data => {
+            game.load(data.fen);
+            var config = {
+                draggable: true,
+                position: data.fen,
+                onDragStart: onDragStart,
+                onDrop: onDrop,
+                pieceTheme: '/static/img/chesspieces/wikipedia/{piece}.png'
+            };
+            board = ChessBoard('board', config);
+        });
+}
